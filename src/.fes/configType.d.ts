c8dec282f2d47c07a0ffa4e770a3db5b87752278
Liu-302 <yumengliu0302@gmail.com>
2026-02-23 21:05:42 +0800

    chore: remove generated src/.fes from tracking

--- src/.fes/configType.d.ts
+++
@@ -1,3 +0,0 @@
-
-export * from '@fesjs/preset-built-in';
-export * from '@fesjs/builder-vite';
--- src/.fes/core/plugin.js
+++
@@ -1,7 +0,0 @@
-import { Plugin } from 'C:/Users/34175/grp-skypath/node_modules/@fesjs/runtime';
-
-const plugin = new Plugin({
-  validKeys: ['modifyClientRenderOpts','rootContainer','onAppCreated','render','patchRoutes','modifyCreateHistory','modifyRoute','beforeRender','onRouterCreated',],
-});
-
-export { plugin };
